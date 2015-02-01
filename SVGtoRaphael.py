d64d596d75366df78c9dbb560a9d5f53128205cd
Minas Giannekas <whiplashoo721@gmail.com>
2015-02-01 12:55:24 +0200

    Update SVGtoRaphael.py

--- SVGtoRaphael.py
+++ SVGtoRaphael.py
@@ -20,6 +20,6 @@ g = root[3]
 ######################
 with open("final.txt","w") as f:
     for path in g:
-        f.write('var '+ path.attrib['id'].replace(".","").replace("-","").replace("+","") + '=rsr.path("'+ path.attrib['d'] + '").attr({id:"'+path.attrib['id'] + '",parent: "layer1",fill:"#d1dbdd","fill-opacity": "1",stroke:"#000000","stroke-width":"1","stroke-miterlimit":"4","stroke-opacity":"1","stroke-dasharray":"none"}).data("id","'+path.attrib['id']+'");')
+        f.write('var '+ path.attrib['id'].replace(".","").replace("-","").replace("+","").replace(" ","_") + '=rsr.path("'+ path.attrib['d'] + '").attr({id:"'+path.attrib['id'] + '",parent: "layer1",fill:"#d1dbdd","fill-opacity": "1",stroke:"#000000","stroke-width":"1","stroke-miterlimit":"4","stroke-opacity":"1","stroke-dasharray":"none"}).data("id","'+path.attrib['id']+'");')
 
 
